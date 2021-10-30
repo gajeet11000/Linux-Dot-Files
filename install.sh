36f459b589f3eec7fa003a10803a0a8a7baea825
G Ajeet <gajeet11000@gmail.com>
2021-10-30 14:22:32 +0530

    add themes install script

--- install.sh
+++ install.sh
@@ -4,15 +4,20 @@ sudo pacman -Syu
 
 sudo pacman -S --noconfirm base-devel
 
-sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd nemo nemo-engrampa fisher yay dconf-editor flameshot
+sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd fisher yay dconf-editor flameshot okular
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo pacman -R --noconfirm midori parole mousepad
 
 yay -S --noconfirm safeeyes teamviewer gammy
 
-cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .local/ .xdman/ .gvimrc .vimrc .bashrc ~/
+cp -r .config/ .fonts/ .icons/ .moc/ .vim/ .local/ .xdman/ .gvimrc .vimrc ~/
 
-sudo pacman -R --noconfirm midori parole mousepad
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git ~/Downloads
+git clone https://github.com/vinceliuice/Orchis-theme.git ~/Downloads
+
+~/Downloads/Tela-circle-icon-theme/./install.sh -a
+sudo ~/Downloads/Orchis-theme/./install.sh -t all
 
 fisher install IlanCosman/tide
 
